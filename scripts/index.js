a825362374e3047ddc647f1a4f9b0e1c40467f14
niyvn <niyvo10@gmail.com>
2025-10-30 00:44:52 -0500

    Final: fix modal open logic and export Api properly

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
-import Api from "./Api.js";
+import Api from "../scripts/Api.js";
 import {
   enableValidation,
   resetValidation,
   validationConfig,
-} from "./validation.js";
+} from "../scripts/validation.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const TOKEN = "a1a53b3b-e020-4563-a1e8-452dd444bb16";
